365a8f76c3520ed5e66c3f302617790318a112fb
abnerrodrigues98 <108356398+abnerrodrigues98@users.noreply.github.com>
2024-07-26 14:14:03 -0300

    fix: improve steps columns order

--- dbt-trustly/models/intermediate/int_sessions.sql
+++ dbt-trustly/models/intermediate/int_sessions.sql
@@ -1,9 +1,9 @@
 SELECT
     session_id,
+    MAX(CASE WHEN step_name = 'INITIATED_LIGHTBOX' THEN session_created_at END) AS initiated_lightbox_created_datetime,
+    MAX(CASE WHEN step_name = 'SELECT_BANK' THEN session_created_at END) AS select_bank_created_datetime,
     MAX(CASE WHEN step_name = 'LOGIN_ATTEMPT' THEN session_created_at END) AS login_attempt_created_datetime,
     MAX(CASE WHEN step_name = 'AUTHORIZATION' THEN session_created_at END) AS authorization_created_datetime,
-    MAX(CASE WHEN step_name = 'SELECT_BANK' THEN session_created_at END) AS select_bank_created_datetime,
-    MAX(CASE WHEN step_name = 'INITIATED_LIGHTBOX' THEN session_created_at END) AS initiated_lightbox_created_datetime,
     COUNT(DISTINCT step_id) AS total_steps,
     MAX(CASE WHEN step_name = 'AUTHORIZATION' THEN 1 ELSE 0 END) AS is_session_complete,
     MAX(session_updated_at) AS last_session_updated_at,
